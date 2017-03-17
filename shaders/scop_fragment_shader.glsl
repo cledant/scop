9f5f6c4a694ce4062ee9cd712417deb78388ccef
cledant <cledant@student.42.fr>
2017-03-17 14:28:01 +0100

    fix alpha on shader when in not texture mode

--- shaders/scop_fragment_shader.glsl
+++ shaders/scop_fragment_shader.glsl
@@ -15,16 +15,20 @@ vec4				color_transition;
 void main (void)
 {
 	alpha_test = texture(tex_tex, vertex_tex);
-	if (var_wiremode == 0 && alpha_test.a < 0.5f)
-			discard;
 	color_transition = vertex_color;
 	if (var_color_transition == 0.0f)
 		color = color_transition;
 	else if (var_color_transition == 1.0f)
+	{
 		color = alpha_test;
+		if (var_wiremode == 0 && alpha_test.a < 0.5f)
+			discard;
+	}
 	else
-	color = alpha_test * var_color_transition +
-		color_transition * (1 - var_color_transition);
+	{
+		color = alpha_test * var_color_transition +
+			color_transition * (1 - var_color_transition);
+	}
 	if (var_wiremode == 1)
 		color.a = 1.0f;
 }
